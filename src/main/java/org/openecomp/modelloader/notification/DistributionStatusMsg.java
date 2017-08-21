cdf54f2734655b468d2f141751c5c45268e31110
Daniel Silverthorn <daniel.silverthorn@amdocs.com>
2017-08-21 13:14:43 -0400

    Remove OpenECOMP from license file
    Change-Id: I76110503a4f74f24bd8ba66c633e1a3cb13fef6a
    Issue-ID: AAI-86
    Signed-off-by: Daniel Silverthorn <daniel.silverthorn@amdocs.com>

--- src/main/java/org/openecomp/modelloader/notification/DistributionStatusMsg.java
+++ src/main/java/org/openecomp/modelloader/notification/DistributionStatusMsg.java
@@ -1,6 +1,6 @@
 /**
  * ============LICENSE_START=======================================================
- * Model Loader
+ * org.onap.aai
  * ================================================================================
  * Copyright © 2017 AT&T Intellectual Property.
  * Copyright © 2017 Amdocs
@@ -9,7 +9,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +19,7 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  *
- * ECOMP and OpenECOMP are trademarks
- * and service marks of AT&T Intellectual Property.
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  */
 package org.openecomp.modelloader.notification;
 
