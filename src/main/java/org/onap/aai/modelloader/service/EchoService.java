a6b110a3a4ffb3280e6d413d0920117b62fe111d
Stela Stoykova <Stela.Stoykova@amdocs.com>
2019-01-09 10:37:13 -0500

    Added echo service to model-loader
    Issue-ID: AAI-2060
    
    Change-Id: I19e16b7447f5ef8b7533449b18895c2bc98188d2
    Signed-off-by: Stela Stoykova <Stela.Stoykova@amdocs.com>

---
+++ src/main/java/org/onap/aai/modelloader/service/EchoService.java
@@ -0,0 +1,38 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 European Software Marketing Ltd.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.modelloader.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/services/model-loader/v1/echo-service")
+public class EchoService {
+
+    @RequestMapping(value = "/echo/{input}", method = RequestMethod.GET)
+    public ResponseEntity<String> echo(@PathVariable String input) {
+      return ResponseEntity.ok(input);
+    }
+
+}
