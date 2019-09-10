fc49a34e3194112064fcd8a285bc2e032ac9995f
rajeevme <rajeev.mehta@amdocs.com>
2019-09-11 00:18:03 +0530

    [AAI-2178] Run container process as non-root
    Issue-ID: AAI-2178
    
    Change-Id: I27ef1334c4fae1c564d4ee62bdd991f9ff2a559a
    Signed-off-by: rajeevme <rajeev.mehta@amdocs.com>

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -37,6 +37,31 @@ if [ -z "$CONFIG_HOME" ]; then
     exit 1
 fi
 
+# Changes related to:AAI-2178
+# Change aai model-loader  container processes to run as non-root on the host
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
+ML_LOGS=/var/log/onap/AAI-ML
+
+if [ $(cat /etc/passwd | grep aaiadmin | wc -l) -eq 0 ]; then
+
+        groupadd aaiadmin -g ${GROUP_ID} || {
+                echo "Unable to create the group id for ${GROUP_ID}";
+                exit 1;
+        }
+        useradd --shell=/bin/bash -u ${USER_ID} -g ${GROUP_ID} -o -c "" -m aaiadmin || {
+                echo "Unable to create the user id for ${USER_ID}";
+                exit 1;
+        }
+fi;
+chown -R aaiadmin:aaiadmin ${MICRO_HOME}
+chown -R aaiadmin:aaiadmin ${AJSC_HOME}
+chown -R aaiadmin:aaiadmin ${ML_LOGS}
+find ${MICRO_HOME}  -name "*.sh" -exec chmod +x {} +
+
+gosu aaiadmin ln -s /logs $MICRO_HOME/logs
+JAVA_CMD="exec gosu aaiadmin java";
+###
 # Some properties are repeated here for debugging purposes.
 PROPS="-DAJSC_HOME=$AJSC_HOME"
 PROPS="$PROPS -DCONFIG_HOME=$CONFIG_HOME"
@@ -60,4 +85,4 @@ fi
 JARFILE=$(ls ./model-loader*.jar);
 
 echo "java $java_runtime_arguments $PROPS -jar $JARFILE"
-java $java_runtime_arguments $PROPS -jar $JARFILE
+${JAVA_CMD}  $java_runtime_arguments $PROPS -jar $JARFILE
