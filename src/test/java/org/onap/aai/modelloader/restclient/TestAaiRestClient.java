75f2a9735e76fd629a61ac3abf6c3b61b47db83a
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2023-11-02 09:08:41 +0100

    Add integration test to test model distribution to aai-resources
    - introduce testcontainers to automatically spin up a resources container when running the TestAaiRestClient
    
    Issue-ID: AAI-3672
    Change-Id: I7aa2ce53f00dcb281caa626e6a8114a2d85a3d8a
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- src/test/java/org/onap/aai/modelloader/restclient/TestAaiRestClient.java
+++ src/test/java/org/onap/aai/modelloader/restclient/TestAaiRestClient.java
@@ -4,6 +4,7 @@
  * ================================================================================
  * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
  * Copyright © 2017-2018 European Software Marketing Ltd.
+ * Copyright © 2023 Deutsche Telekom AG.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,126 +21,93 @@
  */
 package org.onap.aai.modelloader.restclient;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.time.Duration;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.onap.aai.modelloader.config.ModelLoaderConfig;
-import org.onap.aai.modelloader.entity.ArtifactType;
 import org.onap.aai.modelloader.entity.model.ModelArtifact;
-import org.onap.aai.modelloader.entity.model.ModelArtifactParser;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
+@Testcontainers
+@SpringBootTest
 public class TestAaiRestClient {
 
     private static final String MODEL_FILE = "src/test/resources/models/l3-network-widget.xml";
+    private static final String modelInvariantId = "3d560d81-57d0-438b-a2a1-5334dba0651a";
+    private static final String modelVersionId = "9111f20f-e680-4001-b83f-19a2fc23bfc1";
+    private ModelLoaderConfig config;
+    @Autowired RestTemplate restTemplate;
+    private int containerPort;
+    AaiRestClient aaiClient;
+    String modelUrl;
 
-    // This test requires a running A&AI system. To test locally, annotate with org.junit.Test
-    public void testRestClient() throws Exception {
+    private final String image = "nexus3.onap.org:10001/onap/aai-resources:1.12.3";
+
+    @Container
+    public GenericContainer<?> resourcesContainer = new GenericContainer<>(DockerImageName.parse(image))
+            .withExposedPorts(8447)
+            .waitingFor(Wait.forLogMessage(".*Resources MicroService Started.*", 1))
+            .withStartupTimeout(Duration.ofMinutes(2));
+
+    @BeforeEach
+    public void setup() {
+        containerPort = resourcesContainer.getFirstMappedPort();
+        String baseUrl = "https://localhost:" + String.valueOf(containerPort);
+        modelUrl = baseUrl + "/aai/v25/service-design-and-creation/models/model/" + modelInvariantId;
         Properties props = new Properties();
         props.setProperty("ml.distribution.ARTIFACT_TYPES", "MODEL_INVENTORY_PROFILE,MODEL_QUERY_SPEC,VNF_CATALOG");
-        props.setProperty("ml.aai.BASE_URL", "https://localhost:8443");
-        props.setProperty("ml.aai.MODEL_URL", "/aai/v9/service-design-and-creation/models/model/");
+        props.setProperty("ml.aai.BASE_URL", baseUrl);
+        props.setProperty("ml.aai.MODEL_URL", "aai/v*/service-design-and-creation/models/model/");
+        props.setProperty("ml.aai.AUTH_USER", "ModelLoader");
+        props.setProperty("ml.aai.AUTH_PASSWORD", "OBF:1qvu1v2h1sov1sar1wfw1j7j1wg21saj1sov1v1x1qxw");
 
-        ModelLoaderConfig config = new ModelLoaderConfig(props, ".");
-
-        File xmlFile = new File(MODEL_FILE);
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        dbFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        Document doc = dbFactory.newDocumentBuilder().parse(xmlFile);
-
-        NodeList nodesList = doc.getDocumentElement().getChildNodes();
-
-        // Get the model IDs
-
-        // @formatter:off
-        String modelInvariantId =
-                getNodesStream(nodesList)
-                        .filter(childNode -> childNode.getNodeName().equals(ModelArtifactParser.MODEL_INVARIANT_ID))
-                        .findFirst()
-                        .map(Node::getTextContent)
-                        .orElse(null);
-
-        String modelId = getNodesStream(nodesList)
-                .flatMap(n -> getNodesStream(n.getChildNodes()))
-                .filter(childNode -> childNode.getNodeName().equals(ModelArtifactParser.MODEL_VER))
-                .findFirst()
-                .map(n -> n.getChildNodes().item(1).getTextContent())
-                .orElse(null);
-        // @formatter:on
-
-        try {
-            // Build the model artifact
-            ModelArtifact model = new ModelArtifact();
-            model.setModelInvariantId(modelInvariantId);
-            model.setModelVerId(modelId);
-            model.setPayload(readFile(MODEL_FILE));
-            model.setModelNamespace("http://org.openecomp.aai.inventory/v9");
-
-            AaiRestClient aaiClient = new AaiRestClient(config, new RestTemplate());
-
-            // GET model
-            ResponseEntity opResult =
-                    aaiClient.getResource(getUrl(model, config), "example-trans-id-0", MediaType.APPLICATION_XML, String.class);
-            assertEquals(opResult.getStatusCode(), HttpStatus.NOT_FOUND);
-
-            // PUT the model
-            opResult = aaiClient.putResource(getUrl(model, config), model.getPayload(), "example-trans-id-1",
-                    MediaType.APPLICATION_XML, String.class);
-            assertEquals(opResult.getStatusCode(), HttpStatus.CREATED);
-
-            // DELETE the model
-            opResult = aaiClient.getAndDeleteResource(getUrl(model, config), "example-trans-id-3");
-            assertEquals(opResult.getStatusCode(), HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        config = new ModelLoaderConfig(props, ".");
+        aaiClient = new AaiRestClient(config, restTemplate);
     }
 
-    private Stream<Node> getNodesStream(NodeList nodeList) {
-        return IntStream.range(0, nodeList.getLength()).mapToObj(nodeList::item);
-    }
+    @Test
+    public void thatModelCanBeCreated() throws Exception {
+        // Build the model artifact
+        String payload = new String(Files.readAllBytes(Paths.get(MODEL_FILE)));
+        ModelArtifact model = new ModelArtifact();
+        model.setModelInvariantId(modelInvariantId);
+        model.setModelVerId(modelVersionId);
+        model.setPayload(payload);
+        model.setModelNamespace("http://org.onap.aai.inventory/v25");
 
-    static String readFile(String path) throws IOException {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded);
-    }
+        // GET model
+        HttpClientErrorException thrown = assertThrows(HttpClientErrorException.class,
+            () -> {
+                aaiClient.getResource(modelUrl, "example-trans-id-0", MediaType.APPLICATION_XML, String.class);
+            });
+        assertEquals(HttpStatus.NOT_FOUND, thrown.getStatusCode());
 
-    private String getUrl(ModelArtifact model, ModelLoaderConfig config) {
-        String subUrl;
-        if (model.getType().equals(ArtifactType.MODEL)) {
-            subUrl = config.getAaiModelUrl(model.getModelNamespaceVersion()).trim();
-        } else {
-            subUrl = config.getAaiNamedQueryUrl(model.getModelNamespaceVersion()).trim();
-        }
+        ResponseEntity<?> opResult;
+        // PUT the model
+        opResult = aaiClient.putResource(modelUrl, model.getPayload(), "example-trans-id-1",
+                MediaType.APPLICATION_XML, String.class);
+        assertEquals(opResult.getStatusCode(), HttpStatus.CREATED);
 
-        String baseUrl = config.getAaiBaseUrl().trim();
-        if (!baseUrl.endsWith("/") && !subUrl.startsWith("/")) {
-            baseUrl = baseUrl + "/";
-        }
-
-        if (baseUrl.endsWith("/") && subUrl.startsWith("/")) {
-            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
-        }
-
-        if (!subUrl.endsWith("/")) {
-            subUrl = subUrl + "/";
-        }
-
-        return baseUrl + subUrl + model.getModelInvariantId();
+        // DELETE the model
+        opResult = aaiClient.getAndDeleteResource(modelUrl, "example-trans-id-3");
+        assertEquals(opResult.getStatusCode(), HttpStatus.NO_CONTENT);
     }
 }
