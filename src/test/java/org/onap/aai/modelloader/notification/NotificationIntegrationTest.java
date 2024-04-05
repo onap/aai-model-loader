3a2c0c6d551c1d0ebcf2b13390098dea44b1c2b6
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-04-05 11:38:01 +0200

    Model distribution fails with model-loader 1.13.5
    - add @PostConstruct to register sdc-distribution-client on ModelController creation
    - integration test with embedded kafka
    
    Issue-ID: AAI-3818
    Change-Id: I0b5dd118d9145372ddf123319b58829d0ef9275a
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ src/test/java/org/onap/aai/modelloader/notification/NotificationIntegrationTest.java
@@ -0,0 +1,52 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2024 Deutsche Telekom AG Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.modelloader.notification;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+public class NotificationIntegrationTest {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived() 
+      throws Exception {
+        String data = "Smth";
+
+        kafkaTemplate.send(topic, data);
+
+        assertTrue(true);
+    }
+}
