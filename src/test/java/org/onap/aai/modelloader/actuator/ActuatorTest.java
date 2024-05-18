9d93fb4e58c23ddcb252e6812557bb2dbdcd4ce9
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-05-18 19:52:40 +0200

    Add spring-boot actuator to model-loader
    - add actuator that enables liveness probes
    - allow passing in jvm args to the running process (for debugging or profiling)
    - bump version to 1.14.1
    
    Issue-ID: AAI-3852
    Change-Id: Id4b8c5d5402a8eeaf1630a3cdcdfc68471a26a94
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ src/test/java/org/onap/aai/modelloader/actuator/ActuatorTest.java
@@ -0,0 +1,47 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2024 Deutsche Telekom AG Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.modelloader.actuator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ActuatorTest {
+  
+  @Autowired RestTemplate restTemplate;
+  @LocalServerPort
+  private int serverPort;
+
+  @Test
+  public void thatLivenessEndpointReturnsOk() {
+    String url = String.format("http://localhost:%s/actuator/health", serverPort);
+    ResponseEntity<String> entity = restTemplate.getForEntity(url, String.class);
+    assertEquals(entity.getStatusCode(), HttpStatus.OK);
+    assertEquals(entity.getBody(), "{\"status\":\"UP\"}");
+  }
+}
